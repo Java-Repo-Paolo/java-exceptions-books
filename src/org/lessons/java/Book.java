1a7f7a26c0544109414ef53908fb9de688fa62d8
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-24 16:59:10 +0200

    Commit 2 - Mod Main and add errors

--- src/org/lessons/java/Book.java
+++ src/org/lessons/java/Book.java
@@ -10,7 +10,19 @@ public class Book {
     private String editor;
 
     //COSTRUTTORI
-    public Book(String title, int pages, String author, String editor) {
+    public Book(String title, int pages, String author, String editor) throws IllegalArgumentException {
+        if (title == null || title.isBlank()){
+            throw new IllegalArgumentException("The title is missing");
+        }
+        else if (author == null || author.isBlank()){
+            throw new IllegalArgumentException("The author is missing");
+        }
+        else if (editor == null ||editor.isBlank()){
+            throw new IllegalArgumentException("The editor is missing");
+        }
+        else if(pages <= 0){
+            throw new IllegalArgumentException("The number of pages cannot be 0");
+        }
         this.title = title;
         this.pages = pages;
         this.author = author;
@@ -21,33 +33,68 @@ public class Book {
 
     //Titolo
     public String getTitle() {
+        if (title == null || title.isBlank()){
+            throw new IllegalArgumentException("The title is missing");
+        }
         return title;
     }
     public void setTitle(String title) {
+        if (title == null || title.isBlank()){
+            throw new IllegalArgumentException("The title is missing");
+        }
         this.title = title;
     }
 
     //Pagine
     public int getPages() {
+        if(pages <= 0){
+            throw new IllegalArgumentException("The number of pages cannot be 0");
+        }
+        this.pages = pages;
         return pages;
     }
     public void setPages(int pages) {
+        if(pages <= 0){
+            throw new IllegalArgumentException("The number of pages cannot be 0");
+        }
         this.pages = pages;
     }
 
     //Autore
     public String getAuthor() {
+        if (author == null || author.isBlank()){
+            throw new IllegalArgumentException("The author is missing");
+        }
         return author;
     }
     public void setAuthor(String author) {
+        if (author == null || author.isBlank()){
+            throw new IllegalArgumentException("The author is missing");
+        }
         this.author = author;
     }
 
     //Editore
     public String getEditor() {
+        if (editor == null ||editor.isBlank()){
+            throw new IllegalArgumentException("The editor is missing");
+        }
         return editor;
     }
     public void setEditor(String editor) {
+        if (editor == null ||editor.isBlank()){
+            throw new IllegalArgumentException("The editor is missing");
+        }
         this.editor = editor;
+    }
+
+    @Override
+    public String toString() {
+        return "Book {" +
+            "Title = '" + title + '\'' +
+            ", Number of pages = '" + pages + '\'' +
+            ", Author = " + author +
+            ", Editor = " + editor +
+        '}';
     }
 }
--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,5 +1,44 @@
 package org.lessons.java;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many books do you want to add? ");
+        int addBook = Integer.parseInt(scanner.nextLine());
+
+        Book[] bookLibrary = new Book[addBook];
+
+        for (int i = 0; i < bookLibrary.length; i++) {
+            System.out.println("Insert book " + (i + 1));
+
+            try {
+                System.out.print("Title: ");
+                String title = scanner.nextLine();
+                System.out.print("Number of pages: ");
+                String numberPages = scanner.nextLine();
+                System.out.print("Author: ");
+                String author = scanner.nextLine();
+                System.out.print("Editor: ");
+                String editor = scanner.nextLine();
+
+                Book book = new Book(title, Integer.parseInt(numberPages), author, editor);
+                bookLibrary[i] = book;
+            }catch (IllegalArgumentException e){
+                System.out.println("Invalid error: " + e.getMessage());
+                System.out.println();
+                i--;
+                continue;
+            }
+
+        }
+
+        for (Book totalBooks : bookLibrary) {
+            System.out.println(totalBooks);
+        }
+
+        scanner.close();
+    }
 }
